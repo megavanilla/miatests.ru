cd46e71bbfaf47fbc4b35734030471edf48008fb
megavanilla <megavanilla@mail.ru>
2018-07-25 20:14:52 +0300

    add href to github from patterns

--- patterns/mvc/views/pages/patterns/show.php
+++ patterns/mvc/views/pages/patterns/show.php
@@ -1,6 +1,6 @@
 <h2>Применение паттернов</h2>
 <em>На данной странице выводятся только результаты работы паттернов, код данных патернов следует смотреть по ссылке на гитхабе:
-  <a href=""></a></em>
+  <a href="https://github.com/megavanilla/miatests.ru/tree/master/patterns" target="_blank"></a></em>
 <fieldset>
   <legend>Паттерн Singleton(Синглтон):</legend>
   <iframe src="PatternsExample/Patterns/singleton/" frameborder="0"></iframe>
